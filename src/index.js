b923ccd9013a5318ba703b77cae2599c9666bfab
cmaguire27 <c.maguire19@nuigalway.ie>
2021-03-24 13:24:19 +0000

    ficed typo, X working now

--- src/index.js
+++ src/index.js
@@ -13,8 +13,9 @@ class Square extends React.Component {
     render() {
       return (
         <button className="square"
-         onCLick={() => this.setState({value:
-          'X'})}> 
+         onClick={() => this.setState({value:
+          'X'})}
+          > 
             {this.state.value}
         </button>
       );
